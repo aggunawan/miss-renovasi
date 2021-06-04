9a2d90e41e155c35c6f4d2d1d126500649e9e45a
Agung Gunawan <agunggunawan.github@gmail.com>
2021-06-04 20:52:35 +0700

    update failed login attemp trans

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Username belum terdaftar atau Password salah.',
+    'password' => 'Password salah.',
+    'throttle' => 'Terlalu banyak kesalahan dalam memasukan data. Coba lagi dalam :seconds detik.',
 
 ];
