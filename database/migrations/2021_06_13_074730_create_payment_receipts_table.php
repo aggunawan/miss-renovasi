fcd8639d5a911786c6130f43948aead0cd4c19e6
Agung Gunawan <agunggunawan.github@gmail.com>
2021-06-13 08:29:38 +0700

    setup payment receipt

---
+++ database/migrations/2021_06_13_074730_create_payment_receipts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_receipts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained();
+            $table->integer('amount');
+            $table->string('counted');
+            $table->string('note');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_receipts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use App\Http\Controllers\PaymentConfirmationController;
 use Illuminate\Support\Facades\Route;
 
-Route::patch('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'update'])->name('payment-confirmations.update');
-Route::put('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'update'])->name('payment-confirmations.update');
-Route::get('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'show'])->name('payment-confirmations.show');
+Route::middleware(['guest'])
+	->patch('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'update'])
+	->name('payment-confirmations.update');
+	
+Route::middleware(['guest'])
+	->put('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'update'])
+	->name('payment-confirmations.update');
+	
+Route::middleware(['guest'])
+	->get('payment-confirmation/{payment}', [PaymentConfirmationController::class, 'show'])
+	->name('payment-confirmations.show');
+	
 
 Route::redirect('/', '/admin/dashboard');
