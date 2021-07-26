d8f90c6690c778a3245ad42663ac78b1b7f5ae10
Agung Gunawan <agunggunawan.github@gmail.com>
2021-07-26 21:34:43 +0700

    update login page

--- resources/views/vendor/backpack/base/auth/login.blade.php
+++ resources/views/vendor/backpack/base/auth/login.blade.php
@@ -3,58 +3,69 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-12 col-md-8 col-lg-4">
-            <h3 class="text-center mb-4">{{ trans('backpack::base.login') }}</h3>
+            <div class="row justify-content-center">
+                <div class="col-4 mb-4">
+                    <img src="{{ asset('images/logo-small.jpeg') }}" alt="Logo" class="img-fluid rounded">
+                </div>
+            </div>
             <div class="card">
                 <div class="card-body">
-                    <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.login') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group">
-                            <label class="control-label" for="{{ $username }}">{{ config('backpack.base.authentication_column_name') }}</label>
-
-                            <div>
-                                <input type="text" class="form-control{{ $errors->has($username) ? ' is-invalid' : '' }}" name="{{ $username }}" value="{{ old($username) }}" id="{{ $username }}">
-
-                                @if ($errors->has($username))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first($username) }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <h3 class="text-center mb-4">{{ __('Login') }}</h3>
                         </div>
+                        <div class="col-12">
+                            <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.login') }}">
+                                {!! csrf_field() !!}
 
-                        <div class="form-group">
-                            <label class="control-label" for="password">{{ trans('backpack::base.password') }}</label>
+                                <div class="form-group">
+                                    <label class="control-label" for="{{ $username }}">{{ config('backpack.base.authentication_column_name') }}</label>
 
-                            <div>
-                                <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password">
+                                    <div>
+                                        <input type="text" class="form-control{{ $errors->has($username) ? ' is-invalid' : '' }}" name="{{ $username }}" value="{{ old($username) }}" id="{{ $username }}">
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> {{ trans('backpack::base.remember_me') }}
-                                    </label>
+                                        @if ($errors->has($username))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first($username) }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div>
-                                <button type="submit" class="btn btn-block btn-primary">
-                                    {{ trans('backpack::base.login') }}
-                                </button>
-                            </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="password">{{ trans('backpack::base.password') }}</label>
+
+                                    <div>
+                                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password">
+
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember"> {{ trans('backpack::base.remember_me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div>
+                                        <button type="submit" class="btn btn-block btn-primary">
+                                            {{ trans('backpack::base.login') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
             @if (backpack_users_have_email() && config('backpack.base.setup_password_recovery_routes', true))
